1fcb455ab8fc54b6173bcf5e1a6ec73b35b9f20a
anjuvthomas <anjuvthomas@mca.ajce.in>
2018-04-03 14:53:55 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script async="async" src="https://www.google.com/adsense/search/ads.js"></script>
+
+<!-- other head elements from your page -->
+
+<script type="text/javascript" charset="utf-8">
+(function(g,o){g[o]=g[o]||function(){(g[o]['q']=g[o]['q']||[]).push(
+  arguments)},g[o]['t']=1*new Date})(window,'_googCsa');
+</script>
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<title>Real Estate</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/grid.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.4.2.min.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_italic_400-Myriad_Pro_italic_600.font.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/jquery.faded.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+</head>
+<body>
+<div id='afscontainer1'></div>
+
+<script type="text/javascript" charset="utf-8">
+
+  var pageOptions = {
+    "pubId": "pub-9616389000213823", // Make sure this the correct client ID!
+    "query": "hotels",
+    "adPage": 1
+  };
+
+  var adblock1 = {
+    "container": "afscontainer1",
+    "width": "700",
+    "number": 2
+  };
+
+  _googCsa('ads', pageOptions, adblock1);
+
+</script>
+
+        
+<!-- START PAGE SOURCE -->
+<header>
+  <div class="container_16">
+    <div class="logo">
+      <h1><a href="home.php"><strong>HOME</strong> RENTAL</a></h1>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="home.php" class="current">HOME</a></li>
+        <li><a href="reg.php">SIGN UP</a></li>
+        <li><a href="aboutus.php">ABOUT US</a></li>
+        <li><a href="login.php">LOGIN</a></li>
+      </ul>
+    </nav>
+    <div id="faded">
+      <div class="rap"> <a href="#"><img src="images/big-img1.jpg" alt="" width="571" height="398"></a> <a href="#"><img src="images/big-img2.jpg" alt="" width="571" height="398"></a> <a href="#"><img src="images/big-img3.jpg" alt="" width="571" height="398"></a> </div>
+      <ul class="pagination">
+        <li> <a href="#" rel="0"> <img src="images/f_thumb1.png" alt=""> <span class="left"> Villa<br />
+          2007 year<br />
+          5000 sq.ft </span> <span class="right"> Brick, glass<br />
+          3 beds<br />
+          2 baths </span> </a> </li>
+        <li> <a href="#" rel="1"> <img src="images/f_thumb2.png" alt=""> <span class="left"> Villa<br />
+          2009 year<br />
+          3500 sq.ft </span> <span class="right"> Brick, glass<br />
+          5 beds<br />
+          3 baths </span> </a> </li>
+        <li> <a href="#" rel="1"> <img src="images/f_thumb2.png" alt=""> <span class="left"> Villa<br />
+          2009 year<br />
+          3500 sq.ft </span> <span class="right"> Brick, glass<br />
+          5 beds<br />
+          3 baths </span> </a> </li>
+      </ul>
+      </div>
+  </div>
+</header>
+<section id="content">
+  <div class="container_16">
+    <div class="clearfix">
+      <section id="mainContent" class="grid_10">
+        <article>
+          <h2>Welcome Message</h2>
+          <h3>Owning a rental property is a lot like walking a tightrope. You need to keep walking on that thin line and when a gust of wind comes from the left, it’s your job to lean to the right so you balance yourself and don’t fall off.
+
+In your rental property business, “gusts of wind” can take many forms. Thieving employees, bad property managers, natural disasters, fires, economic depressions, and more can throw your investing off kilter and force you to step in and maintain stability. Don’t assume that just because you own some rentals that you can sit back and relax on a beach 365 days a year.
+
+In addition to managing your properties, you must also manage your finances. Keeping accurate records every day, week, month, and year are vital to the continual success of your business.       <article class="last">
+          <h2>New Remodeling Homes</h2>
+          <ul class="img-list clearfix">
+            <li><a href="#"><img src="images/thumb1.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb2.jpg" alt=""></a></li>
+            
+            <li><a href="#"><img src="images/thumb4.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb5.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb6.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb7.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb8.jpg" alt=""></a></li>
+            <li><a href="#"><img src="images/thumb9.jpg" alt=""></a></li>
+          </ul>
+          </section>
+      <aside class="grid_6">
+        <div class="prefix_1">
+          <article>
+            <div class="box">
+              <h2>5 Keys to Home Rental Success</h2>
+              <dl class="accordion">
+                <dt><img src="images/icon1.gif" alt="">Prospecting</a></dt>
+                <dd>Prospecting is the first step in the sales process, which consists of identifying potential customers, aka prospects. The goal of prospecting is to develop a database of likely customers and then systematically communicate with them in the hopes of converting them from potential customer to current customer.</dd>
+                <dt><img src="images/icon2.gif" alt="">Manage Effectively</a></dt>
+                <dd>Owning a rental property is a lot like walking a tightrope. You need to keep walking on that thin line and when a gust of wind comes from the left, it’s your job to lean to the right so you balance yourself and don’t fall off.
+
+In your rental property business, “gusts of wind” can take many forms. Thieving employees, bad property managers, natural disasters, fires, economic depressions, and more can throw your investing off kilter and force you to step in and maintain stability. Don’t assume that just because you own some rentals that you can sit back and relax on a beach 365 days a year.</dd>
+                <dt><img src="images/icon3.gif" alt="">Give Back</a></dt>
+                <dd>Finally, I believe that every investor has a duty and obligation to give back to others, no matter what level of success they have achieved so far. Giving back can be done in multiple ways, but primarily I’d encourage you to look at giving back both educationally and financially.</dd>
+                <dt><img src="images/icon4.gif" alt="">Increase Income</a></dt>
+                <dd>As you move through your investment life, one of your tasks will also be to increase the income that is generated by your rental properties. And I’m not only talking about raising the rent, though that is obviously the largest aspect of increased income.</dd>
+                <dt><img src="images/icon5.gif" alt="">Thank You</a></dt>
+                <dd></dd>
+              </dl>
+            </div>
+          </article>
+          <article class="last">
+            <h2>Find Our Office</h2>
+            <p><img src="images/map1.jpg" alt=""></p>
+            <div class="wrapper">
+              <ul class="list1 grid_3 alpha">
+                <li><a href="#">KERALA</a></li>
+                <li><a href="#">TAMIL NADU</a></li>
+                <li><a href="#">GOA</a></li>
+              </ul>
+              <ul class="list1 grid_2 omega">
+                <li><a href="#"></a></li>
+                <li><a href="#">KARNATAKA</a></li>
+                <li><a href="#">MAHARASHTRA</a></li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </aside>
+    </div>
+  </div>
+</section>
+<footer>
+ 
+</footer>
+<script type="text/javascript">
+$(function () {
+    //accordion
+    $(".accordion dt").toggle(function () {
+        $(this).next().slideDown();
+    }, function () {
+        $(this).next().slideUp();
+    });
+})
+</script>
+<script type="text/javascript"> Cufon.now(); </script>
+<!-- END PAGE SOURCE -->
+</body>
+</html>
